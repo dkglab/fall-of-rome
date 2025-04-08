7a81acf2c03dfad99fa584fa124be92115e4507a
sychic <47618543+Sychic@users.noreply.github.com>
2025-04-08 14:00:58 -0400

    Create utility for adding oxigraph

---
+++ webapp/src/utils/oxigraph.ts
@@ -0,0 +1,10 @@
+import init from "oxigraph/web.js"
+import GraphStore from "../graph-store"
+
+let loadOxigraph = async () => {
+    await init("/web_bg.wasm") // Required to compile the WebAssembly code.
+
+    const store = new GraphStore()
+    await store.load("/data.ttl")
+}
+export default loadOxigraph
