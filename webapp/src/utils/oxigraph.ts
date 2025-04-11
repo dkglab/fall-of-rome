0f66c33336a356a1ffa81c561f2f28cec7776c79
sychic <47618543+Sychic@users.noreply.github.com>
2025-04-11 12:15:37 -0400

    Switch oxigraph function to const

--- webapp/src/utils/oxigraph.ts
+++ webapp/src/utils/oxigraph.ts
@@ -1,7 +1,7 @@
 import init from "oxigraph/web.js"
 import GraphStore from "../graph-store"
 
-let loadOxigraph = async () => {
+const loadOxigraph = async () => {
     await init("/web_bg.wasm") // Required to compile the WebAssembly code.
 
     const store = new GraphStore()
