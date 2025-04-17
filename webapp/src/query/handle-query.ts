b53d422144b589244e2b18d28f5c94b6ee6e3c7c
sychic <47618543+Sychic@users.noreply.github.com>
2025-04-17 09:39:31 -0400

    Implement query handling

--- webapp/src/query/handle-query.ts
+++ webapp/src/query/handle-query.ts
@@ -1,15 +1,40 @@
 import type { QueryUpdatedEvent } from "./query-updated-event";
+import loadGraphStore from "../utils/oxigraph";
+import type GraphStore from "../graph-store";
 
-function handleQuery(event: QueryUpdatedEvent) {
-    const {query, id} = event.detail
-    console.log(query)
-    console.log(id)
+namespace QueryHandler {
+    let _graphstore: Promise<GraphStore> | undefined = undefined
+
+    export function handleQueryEvent(event: QueryUpdatedEvent) {
+        const {query, id} = event.detail
+        console.log(`running query:\n${query}`)
+        handleQuery(query).then(result => {
+            for (const binding of result) {
+                Array.from(binding).forEach(([variable, value]) => {
+                    console.log(variable)
+                    console.log(value.value)
+                })
+            }
+        })
+    }
+
+    function graphstore(): Promise<GraphStore> {
+        if (!_graphstore) {
+            _graphstore = loadGraphStore()
+        }
+        return _graphstore
+    }
+
+    async function handleQuery(queryString: string) {
+        let store = await graphstore()
+        return store.query(queryString)
+    }
 }
 
 function setupQueryListener() {
     let queryContainer = document.getElementById("query-container")!
     queryContainer.addEventListener("query-updated", (event) => {
-        handleQuery(event as QueryUpdatedEvent)
+        QueryHandler.handleQueryEvent(event as QueryUpdatedEvent)
     })
 }
 
