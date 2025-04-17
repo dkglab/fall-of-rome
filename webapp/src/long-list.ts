394f7dae1ea28160a4460121f41898be734b0511
Ryan Shaw <ryanshaw@unc.edu>
2025-04-17 16:17:09 +0000

    Merge upstream changes

--- webapp/src/long-list.ts
+++ webapp/src/long-list.ts
@@ -1,33 +1,33 @@
-import { html, css, LitElement } from "lit";
-import { customElement, property } from "lit/decorators.js";
-import { LitVirtualizer } from "@lit-labs/virtualizer"; // registers <lit-virtualizer>
+import { html, css, LitElement } from "lit"
+import { customElement, property } from "lit/decorators.js"
+import { LitVirtualizer } from "@lit-labs/virtualizer" // registers <lit-virtualizer>
 
-export interface Item {
-  name: string;
-  id: string;
+export type Item = {
+  name: string
+  id: string
 }
 
 @customElement("long-list")
 export default class LongList extends LitElement {
-  static styles = css``;
+  static styles = css``
 
   @property({ type: Array })
-  items: Item[] = [];
+  items: Item[] = []
 
   connectedCallback() {
-    super.connectedCallback();
+    super.connectedCallback()
     if (this.hasAttribute("items")) {
-      (async () => {
-        const url = this.getAttribute("items")!;
-        const response = await fetch(url);
+      ;(async () => {
+        const url = this.getAttribute("items")!
+        const response = await fetch(url)
         if (response.ok) {
-          this.items = await response.json();
+          this.items = await response.json()
         } else {
           throw new Error(
-            `Failed to fetch ${url}: ${response.status} ${response.statusText}`
-          );
+            `Failed to fetch ${url}: ${response.status} ${response.statusText}`,
+          )
         }
-      })();
+      })()
     }
   }
 
@@ -38,6 +38,6 @@ export default class LongList extends LitElement {
         .renderItem=${(item: Item) =>
           html`<div><a href="site/${item.id}.html">${item.name}</a></div>`}
       ></lit-virtualizer>
-    `;
+    `
   }
 }
